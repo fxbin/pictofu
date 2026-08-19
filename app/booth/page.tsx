71bdf2d5e2f67ea87a08833b3bdd36af15bfbde2
大痴小乙 <fxbin123@gmail.com>
2026-08-19 09:54:23 +0800

    refactor: decouple framing control from layout state

--- app/booth/page.tsx
+++ app/booth/page.tsx
@@ -34,7 +34,7 @@ export default async function BoothPage({ searchParams }: BoothPageProps) {
   return (
     <>
       <BoothClient initialPreset={preset} />
-      <PhotoFramingController initialPresetId={preset.id} />
+      <PhotoFramingController />
       <PoseGuideController initialPresetId={preset.id} />
     </>
   );
